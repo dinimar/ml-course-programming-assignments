f5d54bd8a432658319ebe11d6f5211598014a722
Dinir Imameev <dinir.imameev@gmail.com>
2019-08-30 16:00:23 +0500

    feat(ex3): add predict.m solution

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -20,9 +20,16 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+a2 = sigmoid(X*Theta1');
+% Add ones to the a2 data matrix
+a2 = [ones(m, 1) a2];
+a3 = sigmoid(a2*Theta2');
 
-
-
+for i=1:m
+    [useless, p] = max(a3, [], 2);
+end
 
 
 
