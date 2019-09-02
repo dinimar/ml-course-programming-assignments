83f4ad7296d81032eab9482b2146dc26e546316b
Dinir Imameev <dinir.imameev@gmail.com>
2019-09-02 12:26:03 +0500

    feat

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -55,10 +55,10 @@ for i=1:m
 end
 
 % Regularization parameter
-reg_j = (lambda/2/m)*(sum(sum(Theta1(:, 2:end).^2))+sum(sum(Theta2(:, 2:end))));
+reg_j = (lambda/(2*m))*(sum(sum(Theta1(:, 2:end).^2))+sum(sum(Theta2(:, 2:end).^2)));
 
 % Cost function
-J = (1/m)*sum(sum((-Y).*log(h)-(1-Y).*log(1-h)));
+J = (1/m)*sum(sum((-Y).*log(h)-(1-Y).*log(1-h))) + reg_j;
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
