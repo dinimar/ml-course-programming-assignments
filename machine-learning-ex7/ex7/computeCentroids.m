8c31671fa6d3c846fda2c4aaf48086c581336c79
Dinir Imameev <dinir.imameev@gmail.com>
2019-09-30 09:23:54 +0300

    feat(ex7): add solution 2nd task

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -25,11 +25,48 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
+% 
+% C1 = [0, 0];
+% C2 = [0, 0];
+% C3 = [0, 0];
+% K1 = 0;
+% K2 = 0;
+% K3 = 0;
+% 
+% for i=1:m
+%     if idx(i) == 1
+%         C1(1,1) = C1(1,1) + X(i,1);      
+%         C1(1,2) = C1(1,2) + X(i,2);
+%         K1 = K1 + 1;
+%     else
+%         if idx(i) == 2
+%             C2(1,1) = C2(1,1) + X(i,1);
+%             C2(1,2) = C2(1,2) + X(i,2);
+%             K2 = K2 + 1;
+%         else
+%             if idx(i) == 3
+%                 C3(1,1) = C3(1,1) + X(i,1);              
+%                 C3(1,2) = C3(1,2) + X(i,2);
+%                 K3 = K3 + 1;
+%             end
+%         end
+%     end
+% end
+% 
+% centroids(1,1) = C1(1,1) / K1;
+% centroids(1,2) = C1(1,2) / K1;
+% centroids(2,1) = C2(1,1) / K2;
+% centroids(2,2) = C2(1,2) / K2;
+% centroids(3,1) = C3(1,1) / K3;
+% centroids(3,2) = C3(1,2) / K3;
 
-
-
-
-
+for i = 1:K
+    c_i = idx==i;
+    n_i = sum(c_i);
+    c_i_matrix = repmat(c_i,1,n);
+    X_c_i = X .* c_i_matrix;
+    centroids(i,:) = sum(X_c_i) ./ n_i;
+end
 
 
 
